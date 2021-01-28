1837b79bf0762c6b75de59d7baa16dc2fd5611f1
Yacine Hima <300621723298@bluecrest.edu.gh>
2021-01-28 19:01:03 +0000

    question two part 2

--- app/src/main/java/com/example/appfinal/MainActivity.java
+++ app/src/main/java/com/example/appfinal/MainActivity.java
@@ -1,35 +1,50 @@
-package com.example.appfinal;
 import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
+    private TextView resultTextView;
+    private Button simulateButton;
+    private static int stage;
 
-    int[] colors ={0xFFFF0000,0xFF008000,0xFFFFFF00};//rgb values of {RED,GREEN,YELLOW}
-    // setting the message to display on button
-    String[] message={"STOP","GO","WAIT"};
-    // will count how many times button is pressed (used to maintain cycle)
-    int buttonCount=0;
-    // components of the app
-    View label;
-    Button messageButton;
-    //declaring function which will be called when button is pressed
-    public void ChangeTrafficSignal(View view)
-    {
-        //adding to button count
-        buttonCount+=1;
-        buttonCount=buttonCount%3;//so that button count remains under 3 i.e maintain a cycle;
-        //changing the color
-        label.setBackgroundColor(colors[buttonCount]);
-        //changing the message
-        messageButton.setText(message[buttonCount]);
-    }
-
+    private static final int RED = Color.RED;
+    private static final int YELLOW = Color.YELLOW;
+    private static final int GREEN = Color.GREEN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        label=findViewById(R.id.SingalView);
-        messageButton=findViewById(R.id.singalButton);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
+        simulateButton = (Button) findViewById(R.id.simulateButton);
+// initially set button color to red
+        stage = 1;
+        resultTextView.setText(&quot;STOP&quot;);
+        resultTextView.setTextColor(RED);
+        stage++;
+        simulateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(stage == 1)
+                {
+                    resultTextView.setText(&quot;STOP&quot;);
+                    resultTextView.setTextColor(RED);
+                    stage++;
+                }
+                else if(stage == 2)
+                {
+                    resultTextView.setText(&quot;GO&quot;);
+                    resultTextView.setTextColor(GREEN);
+                    stage++;
+
+                }
+                else if(stage == 3)
+                {
+                    resultTextView.setText(&quot;WAIT&quot;);
+                    resultTextView.setTextColor(YELLOW);
+                    stage = 1;
+                }
+            }
+        });
     }
-}
